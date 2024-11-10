22ca5f01f046935b0c7c81f65e737e26d001518d
AndresDev28 <andresdev28@hotmail.com>
2024-11-10 22:34:46 +0100

    build: output files to /docs for GitHub Pages

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,11 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
 const { merge } = require('webpack-merge');
-// eslint-disable-next-line import/extensions
-const path = require('path');
 // eslint-disable-next-line import/extensions
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
   output: {
-    filename: 'bundle.[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/mi-app/', // Ajusta esto al nombre de tu repositorio en GitHub Pages
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
+    publicPath: '/Weather-App',
   },
 });
